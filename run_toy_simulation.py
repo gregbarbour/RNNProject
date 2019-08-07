187f801f72a858fefca0a01ce2582d96d18781b9
Greg Barbour <greg.barbour@hotmail.com>
2019-08-07 17:53:07 +0100

    added jet generator to c and l jets

--- run_toy_simulation.py
+++ run_toy_simulation.py
@@ -43,26 +43,33 @@ def addThetaGaussianError(theta, std=None):
 
 # generate light jets
 # ljets_df = pd.DataFrame()
-# print("generating light jets")
-#
-#
-# for i in range(n_jets):
-#     jet_energy = random.uniform(1e4, 1e5) # energy not pT is uniform 10GeV to 100GeV
-#     ljet = jet.Jet(jet_energy, 1)
-#
-#     #print("Jet energy is " + str(jet_energy))
-#     primary_particles = jet.generateLightJetPrimary(jet_energy)
-#
-#     # create tracks form all visible particles and add to the jet
-#     allparticles = primary_particles
-#
-#     # When track crated from particle, errors are added
-#     for p in allparticles:
-#         tp = track.Track(p, 1)  # Come back to the issue of charge later, for now assume ntrl, qOverP->oneOverP
-#         ljet.addTrack(tp.printParameters())
-#         # print("[d0,z0,phi,theta,qOverP,x,y,z]: "+str(tp.printRepresentation()))
-#
-#     ljets_df = ljets_df.append(ljet.dataAsPD(), ignore_index=True)
+print("generating light jets")
+
+ljets_list = n_jets*[None]
+
+for i in range(n_jets):
+    jet_energy = random.uniform(1e4, 1e5) # energy not pT is uniform 10GeV to 100GeV
+    ljet_creator = jet_generator.JetGenerator(jet_energy, 'light')
+
+    # print("Jet energy is " + str(jet_energy))
+    ljet, primary_particles = ljet_creator.create_jet_container_and_primaries()
+
+    # create tracks form all visible particles and add to the jet
+    allparticles = primary_particles
+
+    # When track crated from particle, errors are added
+    for p in allparticles:
+        tp = track.Track(p, 1)  # Come back to the issue of charge later, for now assume ntrl, qOverP->oneOverP
+        ljet.addTrack(tp.printParameters())
+        # print("[d0,z0,phi,theta,qOverP,x,y,z]: "+str(tp.printRepresentation()))
+
+    # ljets_df = ljets_df.append(ljet.dataAsPD(), ignore_index=True)
+    ljets_list[i] = ljet.data()
+
+ljets_df = pd.DataFrame(ljets_list, columns=["jet_energy", "jet_flavour", "nSecTracks","nTerTracks",
+                                                  "secVtx_x", "secVtx_y", "secVtx_z",
+                                                  "terVtx_x", "terVtx_y", "terVtx_z",
+                                                  "tracks"])
 
 
 # generate b jets
@@ -173,58 +180,71 @@ bjets_df = pd.DataFrame(bjets_list, columns=["jet_energy", "jet_flavour", "nSecT
 # print(df_from_list)
 # print(bjets_df==df_from_list)
 # generate c-jets
-# print("generating c jets")
-#
-# cjets_df = pd.DataFrame()
-# for i in range(n_jets):
-#     jet_energy = random.uniform(1e4, 1e5)
-#     cjet = jet.Jet(jet_energy, 4)
-#
-#     #print("Jet energy is " + str(jet_energy))
-#     candD, primary_particles = jet.generateCJetPrimary(jet_energy)
-#
-#     cDecayMode = np.random.choice(["4pi", "3pi", "2pi"])  # add Dne,Dnepi etc.
-#
-#     if cDecayMode == "4pi":
-#         pion1, pion2, pion3, pion4 = candD.propagateAndDecay("4pions")
-#         pions = [pion1, pion2, pion3, pion4]
-#
-#     if cDecayMode == "3pi":
-#         pion1, pion2, pion3 = candD.propagateAndDecay("3pions")
-#         pions = [pion1, pion2, pion3]
-#
-#     if cDecayMode == "2pi":
-#         pion1, pion2 = candD.propagateAndDecay("2pions")
-#         pions = [pion1, pion2]
-#
-#     cjet.setSecondaryVtx(pion1.origin)
-#
-#     # A simple check to ensure four-mom conservation
-#     sumfourMom = [0., 0., 0., 0.]
-#     for p in pions:
-#         sumfourMom += p.fourMom
-#
-#     #print(np.allclose(sumfourMom, candD.fourMom))
-#     if not (np.allclose(sumfourMom, candD.fourMom)):
-#         print("ERROR")
-#         print(sumfourMom)
-#         print(candD.fourMom)
-#         break
-#
-#     # create tracks form all visible particles and add to the jet
-#     allparticles = primary_particles + pions
-#
-#     for p in allparticles:
-#         tp = track.Track(p, 1)  # Come back to the issue of charge later, for now assume ntrl, qOverP->oneOverP
-#         cjet.addTrack(tp.printParameters())
-#         # print("[d0,z0,phi,theta,qOverP,x,y,z]: "+str(tp.printRepresentation()))
-#
-#     cjets_df = cjets_df.append(cjet.dataAsPD(), ignore_index=True)
-#
+print("generating c jets")
+
+#cjets_df = pd.DataFrame()
+
+cjets_list = n_jets*[None]
+
+for i in range(n_jets):
+    jet_energy = random.uniform(1e4, 1e5)
+    cjet_creator = jet_generator.JetGenerator(jet_energy, 'c')
+
+    # print("Jet energy is " + str(jet_energy))
+    cjet, D_meson, primary_particles = cjet_creator.create_jet_container_and_primaries()
+
+    cDecayMode = np.random.choice(["4pi", "3pi", "2pi"])  # add Dne,Dnepi etc.
+
+    if cDecayMode == "4pi":
+        pion1, pion2, pion3, pion4 = D_meson.propagateAndDecay("4pions")
+        pions = [pion1, pion2, pion3, pion4]
+
+    elif cDecayMode == "3pi":
+        pion1, pion2, pion3 = D_meson.propagateAndDecay("3pions")
+        pions = [pion1, pion2, pion3]
+
+    elif cDecayMode == "2pi":
+        pion1, pion2 = D_meson.propagateAndDecay("2pions")
+        pions = [pion1, pion2]
+
+    else:
+        print("error: no c decay mode selected")
+        break
+
+    cjet.setSecondaryVtx(pion1.origin)
+
+    # A simple check to ensure four-mom conservation
+    sumfourMom = [0., 0., 0., 0.]
+    for p in pions:
+        sumfourMom += p.fourMom
+
+    # print(np.allclose(sumfourMom, candD.fourMom))
+    if not (np.allclose(sumfourMom, D_meson.fourMom)):
+        print("ERROR")
+        print(sumfourMom)
+        print(D_meson.fourMom)
+        break
+
+    # create tracks form all visible particles and add to the jet
+    allparticles = primary_particles + pions
+
+    for p in allparticles:
+        tp = track.Track(p, 1)  # Come back to the issue of charge later, for now assume ntrl, qOverP->oneOverP
+        cjet.addTrack(tp.printParameters())
+        # print("[d0,z0,phi,theta,qOverP,x,y,z]: "+str(tp.printRepresentation()))
+
+    # cjets_df = cjets_df.append(cjet.dataAsPD(), ignore_index=True)
+    cjets_list[i] = cjet.data()
+
+cjets_df = pd.DataFrame(cjets_list, columns=["jet_energy", "jet_flavour", "nSecTracks","nTerTracks",
+                                                  "secVtx_x", "secVtx_y", "secVtx_z",
+                                                  "terVtx_x", "terVtx_y", "terVtx_z",
+                                                  "tracks"])
+
 
 
 # save all results using pickle
 
 bjets_df.to_pickle("./bjets_test.pkl")
-#cjets_df.to_pickle("./cjetsMINERRs.pkl")
-#ljets_df.to_pickle("./ljetsMINERRs.pkl")
+cjets_df.to_pickle("./cjets_test.pkl")
+ljets_df.to_pickle("./ljets_test.pkl")
