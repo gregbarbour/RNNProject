d83ae8cae3e9d472980e86d58f8cb8b94f04fd46
Greg Barbour <greg.barbour@hotmail.com>
2019-08-13 14:31:39 +0100

    timing old method for decays

--- run_toy_simulation.py
+++ run_toy_simulation.py
@@ -4,9 +4,11 @@ import particle
 import jet
 import track
 import random
+import time
 
+start = time.time()
 # This script is run to generate a number of toy jets of three flavours and save them using pickle
-n_jets = 300000
+n_jets = 10000
 
 # the following used to add a gaussian err to the jet kinematic vars
 def addJetVarsGaussianError(parameter, std=None):
@@ -41,27 +43,27 @@ def addThetaGaussianError(theta, std=None):
 
 
 # generate light jets
-# ljets_df = pd.DataFrame()
-# print("generating light jets")
-#
-#
-# for i in range(n_jets):
-#     jet_energy = random.uniform(1e4, 1e5) # energy not pT is uniform 10GeV to 100GeV
-#     ljet = jet.Jet(jet_energy, 1)
-#
-#     #print("Jet energy is " + str(jet_energy))
-#     primary_particles = jet.generateLightJetPrimary(jet_energy)
-#
-#     # create tracks form all visible particles and add to the jet
-#     allparticles = primary_particles
-#
-#     # When track crated from particle, errors are added
-#     for p in allparticles:
-#         tp = track.Track(p, 1)  # Come back to the issue of charge later, for now assume ntrl, qOverP->oneOverP
-#         ljet.addTrack(tp.printParameters())
-#         # print("[d0,z0,phi,theta,qOverP,x,y,z]: "+str(tp.printRepresentation()))
-#
-#     ljets_df = ljets_df.append(ljet.dataAsPD(), ignore_index=True)
+ljets_df = pd.DataFrame()
+print("generating light jets")
+
+
+for i in range(n_jets):
+    jet_energy = random.uniform(1e4, 1e5) # energy not pT is uniform 10GeV to 100GeV
+    ljet = jet.Jet(jet_energy, 1)
+
+    #print("Jet energy is " + str(jet_energy))
+    primary_particles = jet.generateLightJetPrimary(jet_energy)
+
+    # create tracks form all visible particles and add to the jet
+    allparticles = primary_particles
+
+    # When track crated from particle, errors are added
+    for p in allparticles:
+        tp = track.Track(p, 1)  # Come back to the issue of charge later, for now assume ntrl, qOverP->oneOverP
+        ljet.addTrack(tp.printParameters())
+        # print("[d0,z0,phi,theta,qOverP,x,y,z]: "+str(tp.printRepresentation()))
+
+    ljets_df = ljets_df.append(ljet.dataAsPD(), ignore_index=True)
 
 
 # generate b jets
@@ -165,58 +167,60 @@ bjets_df = pd.DataFrame(bjets_list, columns=["jet_energy", "jet_flavour", "nSecT
 # print(df_from_list)
 # print(bjets_df==df_from_list)
 # generate c-jets
-# print("generating c jets")
-#
-# cjets_df = pd.DataFrame()
-# for i in range(n_jets):
-#     jet_energy = random.uniform(1e4, 1e5)
-#     cjet = jet.Jet(jet_energy, 4)
-#
-#     #print("Jet energy is " + str(jet_energy))
-#     candD, primary_particles = jet.generateCJetPrimary(jet_energy)
-#
-#     cDecayMode = np.random.choice(["4pi", "3pi", "2pi"])  # add Dne,Dnepi etc.
-#
-#     if cDecayMode == "4pi":
-#         pion1, pion2, pion3, pion4 = candD.propagateAndDecay("4pions")
-#         pions = [pion1, pion2, pion3, pion4]
-#
-#     if cDecayMode == "3pi":
-#         pion1, pion2, pion3 = candD.propagateAndDecay("3pions")
-#         pions = [pion1, pion2, pion3]
-#
-#     if cDecayMode == "2pi":
-#         pion1, pion2 = candD.propagateAndDecay("2pions")
-#         pions = [pion1, pion2]
-#
-#     cjet.setSecondaryVtx(pion1.origin)
-#
-#     # A simple check to ensure four-mom conservation
-#     sumfourMom = [0., 0., 0., 0.]
-#     for p in pions:
-#         sumfourMom += p.fourMom
-#
-#     #print(np.allclose(sumfourMom, candD.fourMom))
-#     if not (np.allclose(sumfourMom, candD.fourMom)):
-#         print("ERROR")
-#         print(sumfourMom)
-#         print(candD.fourMom)
-#         break
-#
-#     # create tracks form all visible particles and add to the jet
-#     allparticles = primary_particles + pions
-#
-#     for p in allparticles:
-#         tp = track.Track(p, 1)  # Come back to the issue of charge later, for now assume ntrl, qOverP->oneOverP
-#         cjet.addTrack(tp.printParameters())
-#         # print("[d0,z0,phi,theta,qOverP,x,y,z]: "+str(tp.printRepresentation()))
-#
-#     cjets_df = cjets_df.append(cjet.dataAsPD(), ignore_index=True)
-#
+print("generating c jets")
+
+cjets_df = pd.DataFrame()
+for i in range(n_jets):
+    jet_energy = random.uniform(1e4, 1e5)
+    cjet = jet.Jet(jet_energy, 4)
+
+    #print("Jet energy is " + str(jet_energy))
+    candD, primary_particles = jet.generateCJetPrimary(jet_energy)
+
+    cDecayMode = np.random.choice(["4pi", "3pi", "2pi"])  # add Dne,Dnepi etc.
+
+    if cDecayMode == "4pi":
+        pion1, pion2, pion3, pion4 = candD.propagateAndDecay("4pions")
+        pions = [pion1, pion2, pion3, pion4]
+
+    if cDecayMode == "3pi":
+        pion1, pion2, pion3 = candD.propagateAndDecay("3pions")
+        pions = [pion1, pion2, pion3]
+
+    if cDecayMode == "2pi":
+        pion1, pion2 = candD.propagateAndDecay("2pions")
+        pions = [pion1, pion2]
+
+    cjet.setSecondaryVtx(pion1.origin)
+
+    # A simple check to ensure four-mom conservation
+    sumfourMom = [0., 0., 0., 0.]
+    for p in pions:
+        sumfourMom += p.fourMom
+
+    #print(np.allclose(sumfourMom, candD.fourMom))
+    if not (np.allclose(sumfourMom, candD.fourMom)):
+        print("ERROR")
+        print(sumfourMom)
+        print(candD.fourMom)
+        break
+
+    # create tracks form all visible particles and add to the jet
+    allparticles = primary_particles + pions
+
+    for p in allparticles:
+        tp = track.Track(p, 1)  # Come back to the issue of charge later, for now assume ntrl, qOverP->oneOverP
+        cjet.addTrack(tp.printParameters())
+        # print("[d0,z0,phi,theta,qOverP,x,y,z]: "+str(tp.printRepresentation()))
+
+    cjets_df = cjets_df.append(cjet.dataAsPD(), ignore_index=True)
+
 
 
 # save all results using pickle
 
-bjets_df.to_pickle("./bjets_MIN_IPerrs_separate.pkl")
-#cjets_df.to_pickle("./cjetsMINERRs.pkl")
-#ljets_df.to_pickle("./ljetsMINERRs.pkl")
+bjets_df.to_pickle("./bjets_test.pkl")
+cjets_df.to_pickle("./cjets_test.pkl")
+ljets_df.to_pickle("./ljets_test.pkl")
+
+print("runtime = "+str(time.time()-start))
