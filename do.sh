0480dd99112c2f96ec56c2c3dd66b09259cf8edc
Greg Barbour <greg.barbour@hotmail.com>
2021-09-07 16:41:06 +0100

    dosjshs

--- do.sh
+++ do.sh
@@ -6,11 +6,13 @@
 #python RNNJF.py --out order_by_decreasing_qp --split 20000 --trial $t --order_by_feature q/p --reverse
 #python RNNJF.py --out order_by_increasing_r0 --split 20000 --trial $t --use_custom_order r0
 #python RNNJF.py --out order_by_decreasing_r0 --split 20000 --trial $t --use_custom_order r0 --reverse
+#python RNNJF.py --out order_by_increasing_t1 --split 20000 --trial $t --use_custom_order t1
+#python RNNJF.py --out order_by_decreasing_t1 --split 20000 --trial $t --use_custom_order t1 --reverse
 
 
 for t in 1 2 3 4 5
 do
-  python RNNJF.py --out order_by_increasing_t1 --split 20000 --trial $t --use_custom_order t1
-  python RNNJF.py --out order_by_decreasing_t1 --split 20000 --trial $t --use_custom_order t1 --reverse
+  python RNNJF.py --out baseline_20k --split 20000 --trial $t
+  python RNNJF.py --out MinMax_d0 --split 20000 --trial $t
 done
 
