07f500e3e6dcdc9c3bbda421a845127d9835ebaf
grbarbou <gregory.barbour@cern.ch>
2021-09-07 13:57:09 +0100

    do.sh

---
+++ do.sh
@@ -0,0 +1,8 @@
+python RNNJF.py --out order_by_increasing_r0 --split 20000 --trial 1 --use_custom_order r0
+python RNNJF.py --out order_by_increasing_r0 --split 20000 --trial 2 --use_custom_order r0
+python RNNJF.py --out order_by_increasing_r0 --split 20000 --trial 3 --use_custom_order r0
+
+python RNNJF.py --out order_by_decreasing_r0 --split 20000 --trial 1 --use_custom_order r0 --reverse
+python RNNJF.py --out order_by_decreasing_r0 --split 20000 --trial 2 --use_custom_order r0 --reverse
+python RNNJF.py --out order_by_decreasing_r0 --split 20000 --trial 3 --use_custom_order r0 --reverse
+
